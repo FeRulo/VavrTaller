42e6308bc3d8226de56ec48cf8c530ac0efbc113
josepaez <josepaez@seven4n.com>
2018-04-06 12:12:47 -0500

    Creadas las clases y el Collect pero sin implementar

--- src/test/scala/taller/DronSuite.java
+++ src/test/scala/taller/DronSuite.java
@@ -1,24 +1,37 @@
 package taller;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.*;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import serviciosDominio.ServicioDron;
+import sustantivos.*;
+import static sustantivos.Direccion.*;
+import java.util.List;
+
+@RunWith(PowerMockRunner.class)
+//@PrepareForTest(fullyQualifiedNames = "serviciosDominio.ServicioDron")
 public class DronSuite {
 
     @Test
-    public void probarDron(){
-        
-//        Dron dron = new Dron();
-//        String reporte = servicioMover.moverDron(dron,"AADID");
-//        Dron.entregar();
-//        assertEquals(reporte, "(-2,4)" );
+    public void probarIdeaNegocio0(){
+
+        mockStatic(ServicioDron.class);
+
+//        when(ServicioDron.moverDron(new Dron(),"hola mundo"))
+//                .thenReturn(new Dron());
+
+        List<Instruccion> instrucciones = ServicioDron.moverDron(new Dron(),"AAIA");
+
+        assertEquals("I", "" + instrucciones.get(2));
 
     }
+
 }
